737c98cf575e00ee03d54b890e17cb1af535095f
Sophia <sophia.sudiacal@gmail.com>
2020-03-16 11:17:15 -0500

    views walkthrough

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,21 +1,32 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.model.IModel;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPosts(){
-        return "posts index page";
+    public String getPosts(Model model){
+        ArrayList<Post> postList = new ArrayList<>();
+        postList.add(new Post(2, "Second Post", "askdfhkashdfkjahsdf"));
+        postList.add(new Post(3, "Third Post", "some more text..."));
+
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPost(@PathVariable int id){
-        return "view an individual post, id="+id;
+    public String getPost(@PathVariable int id, Model model){
+        Post post1 = new Post(id, "Europa's First Post", "Remote Learning Today!");
+        model.addAttribute("title", post1.getTitle());
+        model.addAttribute("body", post1.getBody());
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
